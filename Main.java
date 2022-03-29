bbbda76c9c1da2ba524c94a53125164015d20985
Patrick <iipajcii@gmail.com>
2022-03-28 22:16:27 -0500

    class(Main): Create Main Class
    The Main class will mainly be used for the setup of the client and
    should not contain much of the client programs logic.

---
+++ Main.java
@@ -0,0 +1,37 @@
+class Main{
+	public static void main(String args[]){
+		System.out.println("            Welcome Life and Death");
+		System.out.println("       ______                                   ");
+		System.out.println("    .-\"      \"-.        .-\"\"\"-.    .-\"\"\"-.    ");
+		System.out.println("   /            \\      /       `..'       \\   ");
+		System.out.println("  |              |    |                    |  ");
+		System.out.println("  |,  .-.  .-.  ,|    |                    |  ");
+		System.out.println("  | )(__/  \\__)( |     \\                  /   ");
+		System.out.println("  |/     /\\     \\|      \\                /    ");
+		System.out.println("  (_     ^^     _)       `\\            /'     ");
+		System.out.println("   \\__|IIIIII|__/          `\\        /'       ");
+		System.out.println("    | \\IIIIII/ |             `\\    /'         ");
+		System.out.println("    \\          /               `\\/'           ");
+		System.out.println("     `--------'                                   ");
+		Game game = new Game();
+		game.menu();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
